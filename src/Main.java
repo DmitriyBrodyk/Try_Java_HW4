ff7663cfc4e164f06a885d6c8fac0bb38f2cc8bc
DmitriyBrodyk <brodyk.dmitriy@gmail.com>
2022-10-18 14:19:44 +0300

    Testing more

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import Lessons.Lecture;
+
+public class Main {
+    public static void main(String[] args) {
+        Lecture Math = new Lecture(1134,"/ How use ++", 1,"Work Hard All Day", "Drink Energetics");
+        Lecture English = new Lecture(223,"/ Past Simple", 2,"Work Hard All Day", "Drink Energetics");
+        Lecture Other1 = new Lecture(311,"/ World Trands", 1,"Work Hard All Day", "Drink Energetics");
+        Lecture.printQ();
+//        /qwewqeqwee
+        Lecture test = new Lecture(1, "12", 1, " 1 ", "1");
+        Lecture test1 = new Lecture(1, "12", 1, " 1 ", "1");
+    }
+}
